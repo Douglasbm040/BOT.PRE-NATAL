802e467d7432b3b1e2b1ec07f05c6bee1035065f
Douglasbm040 <76476827+Douglasbm040@users.noreply.github.com>
2021-07-02 13:42:17 -0300

    estruturação do projeto

---
+++ main/main.py
@@ -0,0 +1,9 @@
+#from Bot_messenger 
+import sys 
+import os
+sys.path.append(os.path.abspath(r"BOT_MESSENGER\\"))
+from Bot_messenger import ola
+
+def main():
+    Bot_messenger.on_marionette
+main()
