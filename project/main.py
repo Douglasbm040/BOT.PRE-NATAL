ff386a85078cf11cbd20f2a20777682d5322afe9
Douglasbm040 <76476827+Douglasbm040@users.noreply.github.com>
2021-07-07 17:00:06 -0300

    Update main.py

--- project/main.py
+++ project/main.py
@@ -2,7 +2,8 @@ from bot import admin
 Bot= admin.admin_bot
 
 def main():
-     for i in range(1,21): 
+     rodadas_de_testes=range(1,21)
+     for i in rodadas_de_testes: 
           print(i,' rodada')
           Bot.startbot()
           Bot.Bot_messenger('EU NAO SOU UM ROBOT','É eu msm')
