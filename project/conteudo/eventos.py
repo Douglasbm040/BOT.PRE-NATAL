2417d8f5e3fc35178e3b8347095f502fcfe5ae1e
Douglasbm040 <76476827+Douglasbm040@users.noreply.github.com>
2021-07-12 13:45:29 -0300

    organização de pastas

---
+++ project/conteudo/eventos.py
@@ -0,0 +1,10 @@
+class assuntos():
+    def realizar_perguntas():
+        print('realizar perguntas')
+
+    def anunciar_avisou():
+        print('anunciar avisou')
+    
+    def transmitir_conteudo():
+        print('transmitir conteudo')
+
--- project/setup.py
+++ project/setup.py
@@ -1,6 +1,6 @@
 def criar_rotina():
     texto=input('nome de adminstrador: ')
-    arquivo = open(r"C:\Users\dougl\Documents\GitHub\BOT.PRE-NATAL\project\agendador.bat", "a")
+    arquivo = open(r"C:\Users\dougl\Documents\GitHub\BOT.PRE-NATAL\project\agendador\agendador.bat", "a")
     arquivo.write('schtasks /create /tn agendamento_do_bot /tr ' + 'C:\\Users\\dougl\\Documents\\GitHub\\BOT.PRE-NATAL\\project\\main\\main.exe' + '/sc minutos' + '/mo 1'+ '/st 13:10'+'/et 13:01'+'/k')
     # schtasks /create /tn Security Script /tr sec.vbs /sc minute /mo 100 /st 17:00 /et 08:00 /k
     print(arquivo)
