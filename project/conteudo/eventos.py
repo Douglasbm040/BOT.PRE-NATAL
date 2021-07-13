61d256cc75cc5422692a4bb3eefae2511d18390d
Douglasbm040 <76476827+Douglasbm040@users.noreply.github.com>
2021-07-13 13:03:18 -0300

    CREATE CRIADOR DE VIDEO

--- project/conteudo/eventos.py
+++ project/conteudo/eventos.py
@@ -1,9 +1,13 @@
+
+
 class assuntos():
     def realizar_perguntas():
         print('realizar perguntas')
+        # COMPARTILHAR O LINK DO GOOGLE FORMULARIOS 
 
     def anunciar_avisou():
         print('anunciar avisou')
+        
     
     def transmitir_conteudo():
         print('transmitir conteudo')
