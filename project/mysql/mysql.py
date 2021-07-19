4d118a9b6f89c3f993e5c992e5846136db284f3e
Douglasbm040 <76476827+Douglasbm040@users.noreply.github.com>
2021-07-19 13:20:27 -0300

    Update mysql.py

--- project/mysql/mysql.py
+++ project/mysql/mysql.py
@@ -1,19 +1,27 @@
-import pymysql
 
-conexao = pymysql.connect(
-    host = 'localhost',
-    user='root',
-    passwd='',
-    database='db',
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate(r"C:\Users\dougl\Downloads\florence-30632-firebase-adminsdk-zloyg-ca1547373c.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 
+# adicionar dados ao banco
+
+doc_ref = db.collection(u'users').document(u'alovelace')
+doc_ref.set({
+    u'first': u'Ada',
+    u'last': u'Lovelace',
+    u'born': 1816
+})
+
+#ler dados 
+
+users_ref = db.collection(u'users')
+docs = users_ref.stream()
+for doc in docs:
+    print(f'{doc.id} => {doc.to_dict()}')
 
 
-)
-
-cursor=conexao.cursor()
-cursor.execute('CREATE DATABASE comnursebot')
-cursor.execute("SHOW TABLES")
-
-for tabelas in cursor:
-    print(tabelas)
